ce4fa7b06ffb04597ddec995058fcafff29ebadf
cseve123 <cseve123@163.com>
2020-11-27 23:49:20 +0800

    data setData event getUserInfo

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,14 +5,74 @@ Page({
    * 页面的初始数据
    */
   data: {
+    msg: '初始化测试数据',
+    userInfo: {}
+  },
 
+  /**
+   * methods 
+   */
+  handleParent() {
+    console.log('parent')
+  },
+  handleChild() {
+    console.log('child')
+  },
+  handleGetUserInfo(res) {
+    console.log(res)
+    if (res.detail.userInfo) {
+      this.setData({
+        userInfo: res.detail.userInfo
+      })
+    }
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    console.log(this.data.msg)
+    this.setData({
+      msg: '修改之后的数据'
+    })
 
+    // 授权以后获取用户信息
+    wx.getUserInfo({
+      success: (res) => {
+        console.log(res)
+        this.setData({
+          userInfo: res.userInfo
+        })
+      },
+      fail(err){
+        console.log(err)
+      }
+    })
+
+    // 数据劫持的原理
+    let sourceData = {
+      name: 'jim',
+      age: 12
+    }
+    let currentData = {}
+    for (const key in sourceData) {
+      if (sourceData.hasOwnProperty(key)) {
+        Object.defineProperty(currentData, key, {
+          get() {
+            return sourceData[key]
+          },
+          set(newVal) {
+            // 不能直接替换当前的值，会死循环
+            // 通过迂回的方式
+            sourceData[key] = newVal
+            console.log(sourceData)
+          }
+        })
+      }
+    }
+    currentData.age = 13
+    console.log(currentData.age)
+    // 数据劫持---end
   },
 
   /**
